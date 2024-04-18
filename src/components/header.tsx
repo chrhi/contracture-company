3ed01bb4af28cea9529cc0c5014f581c362be258
abdellah <Abdellah.chehri14@gmail.com>
2024-04-18 21:19:04 +0200

    push to main

---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+import type { FC } from "react";
+
+interface HeaderProps {}
+
+const header: FC = ({}) => {
+  return <div className="w-full h-[70px] bg-black"></div>;
+};
+
+export default header;
